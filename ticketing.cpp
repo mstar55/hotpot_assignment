59fa08b9edb2e87e528a451614bc4ad332c303df
mstar55 <loojh-wm25@student.tarc.edu.my>
2025-07-11 23:21:06 +0800

    innit

---
+++ ticketing.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+    1. 欢迎来到重庆
+    2. 菜单
+    3. 购物车
+    4. 鱼、鸡肉、卷心菜、菠菜、豆腐、蘑菇、牛肉、猪肉、羊肉
+    5.麻辣、番茄、波霸、南瓜、药材、寿喜烧、日式涮涮锅、味噌、泡菜
+*/
+void chongqing_intro() 
+{
+    cout << "welcome to chongqing" << endl;
+}
+
+int main()
+{
+    chongqing_intro();
+
+    return 0;
+}
