cad030349c89830e8515dc5c47552644b2280456
atrocria <220381535+mstar55@users.noreply.github.com>
2025-08-22 01:04:03 +0800

    updates enums and selection types

--- hotpot.cpp
+++ hotpot.cpp
@@ -3,33 +3,50 @@
 #include <string>
 
 using namespace std;
+namespace{
+    void cleanup()
+    {
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
 
-//enum for menu categories
+    void logo();
+}
+
+//enum for menu categorization
 enum class Category 
 {
-    SoupBase, //0
-    Addons, //1
-    Desserts //2
+    // selection start from 0 - 2
+    SoupBase,
+    Addons,
+    Desserts
+};
+
+enum class HotpotType
+{
+    Singular = 1,
+    Yuanyang = 2,
+    FourFlavor = 3
 };
 
 //menu structure
 struct MenuItem 
 {
-    // members
     string name;
     double price;
     Category category;
 };
 
-// work of art worth 100
-void displayMenu()
-{
-    
-}
-
 int main()
 {
-    MenuItem hotpot_menu[15] = {
+    /*
+    ============
+    Deifnitions
+    ============
+    */
+
+    const int menu_item_amount = 15;
+    MenuItem hotpot_menu[menu_item_amount] = {
         // Soup Bases: 4
         {"Mala Soup Base", 15.90, Category::SoupBase},
         {"Tomato Soup Base", 14.50, Category::SoupBase},
@@ -51,7 +68,21 @@ int main()
         {"Boba Hotpot", 10.90, Category::Desserts},
         {"Ais Cream", 3.00, Category::Desserts}};
 
-    // display menu
+    bool selected = false;
+    bool yuanyang = false;
+    int soup_selection1 = 0;
+    int soup_selection2 = 0;
+
+    /*
+    =================
+    Prompting starts
+    =================
+    */
+
+   //! display logo and introduction
+   // logo();
+
+   //display menu
     cout << "Welcome to Chongqing Spicy Hotpot!" << endl;
     for(const auto &item : hotpot_menu)
     {
@@ -59,23 +90,73 @@ int main()
         cout << item.name << " - RM" << fixed << setprecision(2) << item.price << endl;
     }
 
-    bool selected = false;
-    bool yanyang = false;
+    while(true)
+    {
+        //display hotpot types
+        for(int i = 0; i < hotpot_types; i++)
+        {
+            cout << i+1 << ". " << hotpot_type[i] << "\n";
+        }
+
+        //take selection and see if valid
+        cout << "What type of hotpot do you want? > ";
+        int hotpot_selection = 0;
+        HotpotType hotpot_type = static_cast<HotpotType>(hotpot_selection);
+        
+        if (!(cin >> hotpot_selection) || hotpot_selection <= 0 || hotpot_selection > hotpot_types)
+        {
+            cout << "read again la";
+            cleanup();
+            continue;
+        }
+    }
+
+    // is selection type = yuanyang, single, or 4 flavor?
+    switch (hotpot_type)
+    {
+    case constant expression:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+    if(yuanyang)
+    {
+        while(true)
+        {
+            //basic checks if passes or not
+            cout << "Pick soup base 1 > ";
+            if (!(cin >> soup_selection1) || soup_selection1 <= 0 && soup_selection1 > 4)
+            {
+                cout << "again you.";
+                cleanup();
+                continue;
+            }
+            break;
+        }
+
+        while (true)
+        {
+            cout << "Pick soup base 2 > ";
+            if (!(cin >> soup_selection2))
+            {
+                // soup selection -soup_selection1
+                cout << "again you.";
+                cleanup();
+                continue;
+            }
+            break;
+        }
+    }else if(!yuanyang)
+    {
+
+    }
+
     do
     {
-        cout << "Pick soup base 1 > ";
-        int soup_selection1 = 0;
-        if (!(cin >> soup_selection1) || soup_selection1 <= 0 && soup_selection1 > /*category.soupbase*/)
-        {
-
-        }
-
-        cout << "Pick soup base 2 > ";
-        int soup_selection2 = 0;
-        if (!(cin >> soup_selection2))
-        {
-
-        }
+        
 
         switch (soup_selection1)
         {
@@ -87,7 +168,7 @@ int main()
             break;
         }
         // 1. Display base options
-        // 2. Let user pick a base (one or special case yanyang)
+        // 2. Let user pick a base (one or special case yuanyang)
         // 3. Display addon options (loop)
         // 4. Let user pick as many as they want
         // 5. Display dessert options
@@ -97,7 +178,7 @@ int main()
 
     } while (!selected);
 
-    //allow to use 2 flavor if picked the yanyang base
+    //allow to use 2 flavor if picked the yuanyang base
 
     //
 
